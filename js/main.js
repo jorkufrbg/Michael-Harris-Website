b3eee9c25564344b9a65c7e6a1f4973472097ed4
Georgi Karadzhov <jorkufrbg@gmail.com>
2019-04-16 16:41:59 +0300

    Finished responsive

--- js/main.js
+++ js/main.js
@@ -233,10 +233,87 @@ var initNav = function () {
 
 };
 
+(function ($) {
+
+    $.belowthefold = function (element, settings) {
+
+        var fold = $(window).height() + $(window).scrollTop();
+
+        return fold <= $(element).offset().top - settings.threshold;
+
+    };
+
+
+    $.abovethetop = function (element, settings) {
+
+        var top = $(window).scrollTop();
+
+        return top >= $(element).offset().top + $(element).height() - settings.threshold;
+
+    };
+
+
+    $.rightofscreen = function (element, settings) {
+        var fold = $(window).width() + $(window).scrollLeft();
+        return fold <= $(element).offset().left - settings.threshold;
+    };
+
+
+    $.leftofscreen = function (element, settings) {
+        var left = $(window).scrollLeft();
+        return left >= $(element).offset().left + $(element).width() - settings.threshold;
+    };
+
+
+    $.inviewport = function (element, settings) {
+        return !$.rightofscreen(element, settings) && !$.leftofscreen(element, settings) && !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
+    };
+
+
+    $.extend($.expr[':'], {
+
+        "below-the-fold": function (a, i, m) {
+            return $.belowthefold(a, {
+                threshold: 0
+            });
+        },
+
+        "above-the-top": function (a, i, m) {
+            return $.abovethetop(a, {
+                threshold: 0
+            });
+        },
+
+        "left-of-screen": function (a, i, m) {
+            return $.leftofscreen(a, {
+                threshold: 0
+            });
+        },
+
+        "right-of-screen": function (a, i, m) {
+            return $.rightofscreen(a, {
+                threshold: 0
+            });
+        },
+
+        "in-viewport": function (a, i, m) {
+            return $.inviewport(a, {
+                threshold: 0
+            });
+        }
+    });
+
+})(jQuery);
 
 $document.ready(function () {
     console.log('document ready');
     initNav();
+
+    $('.animated:in-viewport').addClass('in-view');
+    $(window).scroll(function () {
+        $('.animated:in-viewport').addClass('in-view');
+    });
+
     $('.inquiry a').magnificPopup({
         type: 'inline',
         mainClass: 'mfp-zoom-in',
@@ -247,4 +324,5 @@ $document.ready(function () {
         midClick: true
     });
 });
+
 
